9e0487c511a2b96ca706f3f13243f1f428a0e478
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-02-20 23:40:10 -0300

    First working iteration with odd calculations

--- odd_scraper.py
+++ odd_scraper.py
@@ -1,6 +1,4 @@
 # Script to scrape through oddspedia.com looking for good arbitrage betting odds (< 1.00) and notifying of the bets found
-import requests
-import sys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -8,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://oddspedia.com/br/futebol/odds"
-headers = {'Accept-Encoding': 'identity','User-Agent':'Chrome'}
 
 service = Service()
 options = webdriver.ChromeOptions()
@@ -16,11 +13,21 @@ d = webdriver.Chrome(service=service, options=options)
 wait = WebDriverWait(d,5)
 
 d.get(url)
+wait.until(EC.presence_of_element_located((By.CLASS_NAME,"nav-calendar-mobile__next")))
+OddsNav = d.find_element(By.CLASS_NAME,"nav-calendar-mobile__next").click()
+
 wait.until(EC.presence_of_element_located((By.CLASS_NAME,"match-odds")))
 matches = d.find_elements(By.CLASS_NAME,"match-odds")
 
 for match in matches:
-    odds = d.find_elements(By.CLASS_NAME,"bookmaker-link cursor-pointer odd")
-    for odd in odds:
-        value = d.find_element(By.CLASS_NAME,'odd__value')
-        print(value.text)
+    if("ODDS INDISPON" in match.text):
+        continue
+
+    rolling_sum = 0
+    print("Match: ",end='')
+
+    match_values = match.text.splitlines()
+    for value in match_values:
+        rolling_sum += (1/float(value))
+        print(f"{value} ",end='')
+    print(f"Odds: {rolling_sum}")
