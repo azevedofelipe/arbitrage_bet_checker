a120c8780bdc50ea9e4a1b15e949974050100954
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-03-01 01:27:17 -0300

    Learned about modules a bit more

--- arb_calculator.py
+++ arb_calculator.py
@@ -1,31 +1,28 @@
 
-def round_to_multiple(number, multiple):
-    return multiple * round(number/multiple)
+def main():
+    pass
 
-
-def calculator(odds):
-    unbiased_bet = {}   #Stores the amount to bet for each outcome for even profit
-    
-    bet_amount = int(input("Enter the amount you wish to bet on this match: "))
-    #bias = input()
-
-    for odd in odds:
-        bet_outputs = []
-        odd_cumulative = 0
-        # Calculates the number to divide bet_amount by
-        for odd2 in odds:
-            odd_cumulative += odd / odd2
-            initial_bet = bet_amount/odd_cumulative
+if __name__ == '__main__':
+    main()
+else:
+    def calculator(odds):
+        unbiased_bet = {}   #Stores the amount to bet for each outcome for even profit
         
-        initial_bet = round_to_multiple(initial_bet,5)
-        bet_outputs.append(round(initial_bet,2))
+        bet_amount = int(input("Enter the amount you wish to bet on this match: "))
+        #bias = input()
 
-        bet_outputs.append(round((initial_bet * odd) - bet_amount,2))
-        unbiased_bet[odd] = bet_outputs
-        unbiased_bet['Total'] = sum(bet_outputs)
+        for odd in odds:
+            bet_outputs = []
+            odd_cumulative = 0
+            # Calculates the number to divide bet_amount by
+            for odd2 in odds:
+                odd_cumulative += odd / odd2
+                initial_bet = bet_amount/odd_cumulative
+            
+            bet_outputs.append(round(initial_bet,2))
 
-    return(unbiased_bet)
+            bet_outputs.append(round((initial_bet * odd) - bet_amount,2))
+            unbiased_bet[odd] = bet_outputs
 
-dict_odds = calculator([2.3,3.35,4.4,50,70])
-print(dict_odds)
-    
+        return(unbiased_bet)
+        
