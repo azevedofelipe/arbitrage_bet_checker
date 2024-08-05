89a1a426fb97f3fa80653dae2bca57e106c3aa1f
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-08-05 01:20:18 -0300

    Feat: Add list_all_days() to list range of days, add try catch to api calls that dont return json

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,12 @@
 import pytest
-from utils import format_date
+import sys
+import os
+from datetime import date,timedelta
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
+from utils import format_date,list_all_days
 
 @pytest.mark.parametrize("input_date,expected", [
     ('2024-07-30 00:00:00+00', '30/07 12 AM'),
@@ -9,3 +16,11 @@ from utils import format_date
 ])
 def test_format_date(input_date, expected):
     assert format_date(input_date) == expected
+
+
+@pytest.mark.parametrize("start_date, end_date, expected", [
+    (date(2024,8,4),date(2024,8,5),{'2024-08-04':'2024-08-05','2024-08-05': '2024-08-06'}),
+    (date(2024,8,4),date(2024,8,7),{'2024-08-04':'2024-08-05','2024-08-05':'2024-08-06','2024-08-06':'2024-08-07','2024-08-07':'2024-08-08'})
+])
+def test_list_dates(start_date,end_date,expected):
+    assert list_all_days(start_date,end_date) == expected
--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 import json
 import re
 import pandas as pd
+from datetime import date,timedelta
+from selenium.common.exceptions import NoSuchElementException
 
 
 def create_driver():
@@ -19,7 +21,13 @@ def call_api(driver, url: str):
     
     try:
         # wait.until(EC.presence_of_element_located((By.TAG_NAME,'pre')))
-        pre_element = driver.find_element(By.TAG_NAME, 'pre').text
+        try:
+            pre_element = driver.find_element(By.TAG_NAME, 'pre').text
+
+        except NoSuchElementException:
+            logger.log("<pre> element not found")
+            return None
+
         logger.log("Found JSON")
         json_data = json.loads(pre_element)
         logger.log('Returned JSON')
@@ -34,3 +42,14 @@ def format_date(date_str: str) -> str:
     dt = pd.to_datetime([date_str])
     formatted_date = dt.strftime('%d/%m %I %p')
     return formatted_date[0]
+
+# Grabs all days upto end_date + 1 to filter urls
+def list_all_days(start_date: date, end_date: date) -> dict[str,str]:
+    current_date = start_date
+    date_dict = {}
+
+    while current_date <= end_date: 
+        date_dict[str(current_date)] = str(current_date + timedelta(days=1))
+        current_date += timedelta(days=1)
+    
+    return date_dict
