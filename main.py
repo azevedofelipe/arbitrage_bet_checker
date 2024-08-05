32aa60d3f1c50626cd5e60d637d930708553f9e1
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-08-04 23:40:03 -0300

    Feat: Add very basic time tracking

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from datetime import date,timedelta
 from match_odds import MatchOdds
 from logger import logger
 import pandas as pd
+import time
 
 
-TODAY = str(date.today())
-TMRW = str(date.today() + timedelta(days=1))
+TODAY = date.today()
+TMRW = date.today() + timedelta(days=1)
 
 
 def main():
-    football_profit = MatchOdds('football',TODAY,TMRW,0,'prematch')
+    start = time.time()
+    football_profit = MatchOdds('football',TODAY,TMRW,1,'prematch')
     
     if  isinstance(football_profit.df, pd.DataFrame):
         logger.log('Found profitable matches')
         football_profit.display_matches()
-        print(football_profit.profitable)
+    
+    print(f'Took {time.time() - start} seconds')
 
     #TODO Look into wait on call_api why it takes so long
     #TODO Allow user to select start and end dates
