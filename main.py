664685093914c1c6bd1b7479b20c50bd3b211442
azevedofelipe <felipe.azevedo@dna.tec.br>
2024-08-19 02:30:29 -0300

    Refactor: Remove main.py for now, use GUI.py

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
-from datetime import date,timedelta
-from match_odds import MatchOdds
-from utils.logger import logger
-import pandas as pd
-import time
+# from datetime import date,timedelta
+# from match_odds import MatchOdds
+# from utils.logger import logger
+# import pandas as pd
+# import time
 
 
-START = date.today()
-END = date.today() + timedelta(days=1)
+# START = date.today()
+# END = date.today() + timedelta(days=1)
 
 
-def main():
-    start = time.time()
-    football_profit = MatchOdds('football',START,END,1,'prematch')
+# def main():
+#     start = time.time()
+#     football_profit = MatchOdds('football',START,END,1,'prematch')
     
-    if  isinstance(football_profit.df, pd.DataFrame):
-        logger.log('Found profitable matches')
-        football_profit.display_matches()
+#     if  isinstance(football_profit.df, pd.DataFrame):
+#         logger.log('Found profitable matches')
     
-    print(f'Took {time.time() - start} seconds')
+#     print(f'Took {time.time() - start} seconds')
 
-    #TODO Look into wait on call_api why it takes so long
-    #TODO Allow user to select start and end dates
-    #TODO look into how to run multiple drivers at once
-    #TODO remake requirements.txt
+#     #TODO remake requirements.txt
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
